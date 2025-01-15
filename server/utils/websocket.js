15f041ac320831935996d9b789faf366fb56358c
Alphanikhil <nikku696969@gmail.com>
2025-01-15 04:04:58 -0800

    Update websocket.js

--- server/utils/websocket.js
+++ server/utils/websocket.js
@@ -1,16 +1,13 @@
 import { WebSocketServer } from 'ws';
 import qrcode from 'qrcode-terminal';
 
+let wss;
+
 const initializeWebSocket = (server) => {
-    const wss = new WebSocketServer({ server });
+    wss = new WebSocketServer({ server });
 
     wss.on('connection', (ws) => {
         console.log('Client connected');
-
-        // Generate and send QR code data
-        const qrCodeData = 'Hello, World!'; // Replace with your dynamic data
-        ws.send(qrCodeData);
-        qrcode.generate(qrCodeData, { small: true });
 
         ws.on('close', () => {
             console.log('Client disconnected');
@@ -26,4 +23,4 @@ const initializeWebSocket = (server) => {
     console.log('WebSocket server is running');
 };
 
-export default initializeWebSocket;
+export { initializeWebSocket, wss };
